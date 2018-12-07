3f187fc60b23b60626c0f14645033be953e28605
SmirnovA <jumpup@mail.ru>
2018-12-07 18:53:45 +0300

    HM1 - The third test

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,12 +1,16 @@
 public class MainClass {
 
     private int localNumber = 14;
-    private  int class_number = 20;
+    private int class_number = 20;
+    private String class_string = "Hello, world";
+
+    public String getClassString(){
+        return  class_string;
+    }
 
     public int getClassNumber(){
         return class_number;
     }
-
     public int getLocalNumber(){
         return localNumber;
     }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -6,11 +6,17 @@ public class MainClassTest extends  MainClass{
     private int testGetLocalNumber;
 
     @Test
+    public  void testGetClassString(){
+        boolean isString;
+        String result = getClassString();
+        Assert.assertTrue("метод getClassNumber не возвращает строку, в которой есть подстрока “hello” или “Hello”", result.contains("Hello") || result.contains("Hello"));
+    }
+
+    @Test
     public void testGetClassNumber(){
 
             Assert.assertTrue("метод MainClass.getClassNumber() returns number less than 45",getClassNumber()>45 );
     }
-
     @Test
     public void checkGetLocalNumber(){
 
